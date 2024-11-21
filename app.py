017fb74c1e2b51963a2733843d4e2f6e9a70b178
coder7475 <robiulhossain7475@gmail.com>
2024-11-21 12:54:55 +0600

    url building

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
 @app.route("/user/<name>")
 def hello_user(name):
@@ -28,3 +24,22 @@ def projects():
 @app.route('/about')
 def about():
     return 'The about page'
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
