1cb46b2964b1732c062f27ea83105220e2976966
coder7475 <robiulhossain7475@gmail.com>
2024-11-21 12:43:41 +0600

    add several routes

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}"
+@app.route("/user/<name>")
+def hello_user(name):
+    return f"User, {escape(name)}"
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
